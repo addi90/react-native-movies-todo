3cb79eb0e290ad86e1353e5ecbd7432eead30152
Aditya Singh <adisinghrajput@gmail.com>
2016-03-13 13:29:53 +0530

    Add Moviews todo checklist component

---
+++ app/components/moviestodo/moviestodo.js
@@ -0,0 +1,105 @@
+'use strict';
+
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ListView,
+} from 'react-native';
+import { Checkbox } from 'react-native-material-design';
+
+const REQUEST_ENDPOINT = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=7waqfqbprs7pajbz28mqf6vz&page_limit=50';
+
+class MoviesTodo extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData () {
+    fetch(REQUEST_ENDPOINT)
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(response.movies.map(this.transformMovie)),
+          loaded: true
+        })
+      });
+  }
+
+  transformMovie (movie) {
+    const { title, id } = movie;
+    return {
+      isDone: false,
+      text: title,
+      value: id,
+    }
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderView}
+          style={styles.listView}
+        />
+    );
+  }
+
+  renderLoadingView () {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading TODOs...
+        </Text>
+      </View>
+    );
+  }
+
+  _markDone = () => {
+    this.setState({ isDone: true })
+  };
+
+  renderView = (task) => {
+    const { isDone, text, value } = task;
+    return (
+      <View style={styles.container}>
+        <Checkbox
+          value={ value }
+          checked={ isDone }
+          label={ text }/>
+      </View>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+module.exports = MoviesTodo;
